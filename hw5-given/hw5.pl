bfbd0d38895c6550a103c9e1e8236ee2a6afd67c
Kevin Chung <kechung@ucdavis.edu>
2017-12-06 21:12:26 -0800

    just transferring some more work

--- hw5-given/hw5.pl
+++ hw5-given/hw5.pl
@@ -14,4 +14,21 @@ prereq_110(P) :-
 
 ecs140a_students(N) :-
 	student(N,C),
-	member(ecs140a,C).	
+	member(ecs140a,C).
+
+instructor_names(I) :-
+	student(john,T),
+	instructor(I,E),
+	check(E,T).
+
+students(S) :-
+	instructor(jim,E),
+	student(S,T),
+	check(T,E).
+
+check(L1,L2) :-
+	member(X,L1),
+	member(X,L2),!.
+
+allprereq(C,PreList) :-
+	course().
